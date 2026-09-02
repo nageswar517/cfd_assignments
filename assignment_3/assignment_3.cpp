497956029c7d36c84eb4dea8643350475653244b
nageswar517 <nageswar.dvsg@gmail.com>
2026-09-02 21:52:13 +0530

    assignment 3 init

--- assignment_3/assignment_3.cpp
+++ assignment_3/assignment_3.cpp
@@ -5,20 +5,8 @@
 
 using namespace std;
 
-int main(int argc, char** argv){
-    if(argc < 3) {
-        cout << "Usage .\assignment_3.exe <a.dat> <b.dat>\n";
-        return 0;
-    }
-
+void ReadFileToMat(char* file, vector<vector<float>>& mat){
     ifstream in;
-    in.open(argv[1]);
-    if(!in.is_open()) {
-        cout << "Error opening file " << argv[1] << ".\n";
-        return 1;
-    }
-
-    vector<vector<float>> a, b;
     string line;
     
     while(getline(in, line)){
@@ -29,35 +17,34 @@ int main(int argc, char** argv){
         while(ss >> num){
             temp.push_back(num);
         }
-        a.push_back(temp);
+        mat.push_back(temp);
     }
     in.close();
+}
 
-    in.open(argv[2]);    
-    while(getline(in, line)){
-        vector<float> temp;
-        stringstream ss(line);
-        float num;
-        while(ss >> num){
-            temp.push_back(num);
-        }
-        b.push_back(temp);
+int main(int argc, char** argv){
+    if(argc < 3) {
+        cout << "Usage .\assignment_3.exe <a.dat> <b.dat>\n";
+        return 0;
     }
-    in.close();
 
+    vector<vector<float>> a, b;
+
+    ReadFileToMat(argv[1], a);
     for(int i=0; i<a.size(); i++){
         for(int j=0; j<a[0].size(); j++){
             cout << a[i][j] << "  ";
         }
         cout << "\n";
     }
+
+    ReadFileToMat(argv[2], b);
     for(int i=0; i<b.size(); i++){
         for(int j=0; j<b[0].size(); j++){
             cout << b[i][j] << "  ";
         }
         cout << "\n";
     }
-
     
     return 0;
 }
