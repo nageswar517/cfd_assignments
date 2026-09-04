57f3b2ea3eac6bb3707d00eb9316dd3e6daa31b8
nageswar517 <nageswar.dvsg@gmail.com>
2026-09-04 22:49:05 +0530

    assignment 3 final

--- assignment_3/assignment_3.cpp
+++ assignment_3/assignment_3.cpp
@@ -8,6 +8,10 @@ using namespace std;
 void ReadFileToMat(char* file, vector<vector<float>>& mat){
     ifstream in;
     in.open(file);
+    if(!in.is_open()){
+        cout << "Error reading file " << file << "\n";
+        exit(1);
+    }
     string line;
     
     while(getline(in, line)){
@@ -23,29 +27,74 @@ void ReadFileToMat(char* file, vector<vector<float>>& mat){
     in.close();
 }
 
+int WriteMatToFile(vector<vector<float>>& mat){
+    ofstream out;
+    out.open("c.dat");
+    if(!out.is_open()){
+        cout << "Error writing to output file.\n";
+        return 1;
+    }
+    for(int i=0; i<mat.size(); i++){
+        for(int j=0; j<mat[0].size(); j++){
+            out << mat[j][i] << " ";
+        }
+        out << "\n";
+    }
+    out.close();
+    cout << "Matrix saved successfully.\n";
+    return 0;
+}
+
+void ScaleAndAddVector(vector<float>& out, float s, vector<float>& col){
+    for(int i=0; i<col.size(); i++){
+        out[i] += s * col[i];
+    }
+}
+
 int main(int argc, char** argv){
     if(argc < 3) {
-        cout << "Usage .\assignment_3.exe <a.dat> <b.dat>\n";
+        cout << "Usage .\\assignment_3.exe <a.dat> <b.dat>\n";
+        return 1;
+    }
+
+    vector<vector<float>> arw, brw;
+
+    ReadFileToMat(argv[1], arw);
+    ReadFileToMat(argv[2], brw);
+
+    vector<vector<float>> a(arw[0].size(), vector<float>(arw.size()));
+    vector<vector<float>> b(brw[0].size(), vector<float>(brw.size()));
+    for(int i=0; i<arw.size(); i++){
+        for(int j=0; j<arw[0].size(); j++){
+            a[j][i] = arw[i][j];
+        }
+    }
+    for(int i=0; i<brw.size(); i++){
+        for(int j=0; j<brw[0].size(); j++){
+            b[j][i] = brw[i][j];
+        }
+    }
+
+    int ra, ca, rb, cb;
+
+    ra = a[0].size(); ca = a.size();
+    rb = b[0].size(); cb = b.size();
+
+    if(ca != rb){
+        cout << "Matrices cannot be multiplied.\n";
         return 0;
     }
 
-    vector<vector<float>> a, b;
-
-    ReadFileToMat(argv[1], a);
-    for(int i=0; i<a.size(); i++){
-        for(int j=0; j<a[0].size(); j++){
-            cout << a[i][j] << "  ";
+    vector<vector<float>> c(cb, vector<float>(ra));
+    for(int i=0; i<cb; i++){
+        vector<float> temp(ra, 0);
+        for(int j=0; j<rb; j++){
+            ScaleAndAddVector(temp, b[i][j], a[j]);
         }
-        cout << "\n";
+        c[i] = temp;
     }
 
-    ReadFileToMat(argv[2], b);
-    for(int i=0; i<b.size(); i++){
-        for(int j=0; j<b[0].size(); j++){
-            cout << b[i][j] << "  ";
-        }
-        cout << "\n";
-    }
+    WriteMatToFile(c);
         
     return 0;
 }
